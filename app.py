1a9da0f0319d1fc3475b3a96b09393a82a6d70a9
SangHoon Lee <s20604@naver.com>
2024-03-20 18:14:41 +0900

    카테고리 검색기능 추가

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import subprocess
+import json
+
+def load_category_data(filepath):
+    categories = []
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            category = json.loads(line.strip())
+            categories.append(category)
+    return categories
 
 app = Flask(__name__)
+categories = load_category_data('./디지털,가전 카테고리.csv')  # CSV 파일 경로 지정
 
 @app.route('/')
 def home():
@@ -23,6 +33,13 @@ def submit():
     except subprocess.CalledProcessError as e:
       error_message = e.stderr
       return render_template('error.html', error=error_message)
+    
+@app.route('/search_category')
+def search_category():
+    query = request.args.get('query', '').lower()
+    results = [cat for cat in categories if query in cat['name'].lower() and cat['last'] == "True"]
+    return jsonify(results[:5])  # 상위 5개 결과만 반환
+
 
 if __name__ == '__main__':
   app.run(debug=True)
