f5f13083d8496558b4c8e06aa2e0a1265c02fa15
SangHoon Lee <s20604@naver.com>
2024-03-21 16:19:19 +0900

    안정화

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import subprocess
 import json
 
 def load_category_data(filepath):
-    categories = []
-    with open(filepath, 'r', encoding='utf-8') as file:
-        for line in file:
-            category = json.loads(line.strip())
-            categories.append(category)
-    return categories
+  categories = []
+  with open(filepath, 'r', encoding='utf-8') as file:
+    for line in file:
+      category = json.loads(line.strip())
+      categories.append(category)
+  return categories
 
 app = Flask(__name__)
 categories = load_category_data('./디지털,가전 카테고리.csv')  # CSV 파일 경로 지정
@@ -22,23 +22,24 @@ def submit():
   if request.method == 'POST':
     amazon_url = request.form['amazon_url']
     productTitle = request.form['productTitle']
-    leafCategoryID = request.form['leafCategoryID']
-        
+    productPrice = request.form['productPrice'] # productPrice 값을 추출
+    leafCategoryID = request.form['selectedCategoryId']
+    
     # subprocess를 사용하여 main.py 실행, stdout과 stderr를 캡처
     try:
-      result = subprocess.run(['python3', 'main.py', amazon_url, productTitle, leafCategoryID],
+      result = subprocess.run(['python3', 'main.py', amazon_url, productTitle, productPrice, leafCategoryID],
                               text=True, capture_output=True, check=True)
       output = result.stdout
       return render_template('result.html', result=output)
     except subprocess.CalledProcessError as e:
       error_message = e.stderr
       return render_template('error.html', error=error_message)
-    
+
 @app.route('/search_category')
 def search_category():
-    query = request.args.get('query', '').lower()
-    results = [cat for cat in categories if query in cat['name'].lower() and cat['last'] == "True"]
-    return jsonify(results[:5])  # 상위 5개 결과만 반환
+  query = request.args.get('query', '').lower()
+  results = [cat for cat in categories if query in cat['name'].lower() and cat['last'] == "True"]
+  return jsonify(results[:5])  # 상위 5개 결과만 반환
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import sys
 from math import ceil
 from dotenv import load_dotenv
@@ -17,14 +16,6 @@ from lib.crawling.ProductDescription import ProductDescription
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
-def load_category_data(filepath):
-    categories = []
-    with open(filepath, 'r', encoding='utf-8') as file:
-        for line in file:
-            category = json.loads(line.strip())
-            categories.append(category)
-    return categories
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -46,7 +37,7 @@ def main(amazon_url, productTitle, productPrice, leafCategoryId):
   with webdriver.Chrome(options=chrome_options) as driver:
     driver.get(amazon_url)
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "a-price-whole")))
+    wait.until(EC.presence_of_element_located((By.ID, "landingImage")))
     soup = BeautifulSoup(driver.page_source, "html.parser")
 
   extractProduct = ExtractProduct(soup)
@@ -54,6 +45,7 @@ def main(amazon_url, productTitle, productPrice, leafCategoryId):
   # productTitle = extractProduct.getTitle() # 제품명
   image_url = extractProduct.getImage() # 제품이미지
   # productPrice = extractProduct.getPrice() # 제품가격
+  productPrice = float(productPrice)
   productInfo = extractProduct.getInfo()  # 제품정보
 
   productDescription = ProductDescription(amazon_url)
@@ -71,29 +63,27 @@ def main(amazon_url, productTitle, productPrice, leafCategoryId):
 
   # 판매이익
   profit = round(productPrice * 0.15, 2)
-    
+
   item_weight = productInfo['품목 무게']
   if item_weight:
-      weight = item_weight.split()
-      dimensions = int(ceil(float(weight[0])))
-      weight_unit = weight[1]
+    weight = item_weight.split()
+    dimensions = int(ceil(float(weight[0])))
+    weight_unit = weight[1]
   else:
-      dimensions = int(reduce(mul, ExtractProduct.extract_numbers_from_string(product_info['제품 크기'])))
-      
+    item_dimensions = ExtractProduct.extract_numbers_from_string(productInfo['제품 크기'])
+    dimensions = int(reduce(mul, item_dimensions))
+
   calculator = Calculator(
       weight_unit=weight_unit,
-      price=product_price,
+      price=productPrice,
       profit=profit
   )
 
-
   dimensions = calculator.calculate_lb(dimensions)
-  print(f"국제배송비는 {dimensions} 입니다.")
+  # print(f"국제배송비는 {dimensions} 입니다.")
 
   tax, vat = calculator.calculate_taxAndvat(dimensions)
-  print(f"관세 : {tax}, 부가세 : {vat} 입니다.")
-
-
+  # print(f"관세 : {tax}, 부가세 : {vat} 입니다.")
 
   # 스마트스토어 판매가
   smart_store_price = calculator.calculate_ssp(
@@ -126,7 +116,7 @@ def main(amazon_url, productTitle, productPrice, leafCategoryId):
     f.write('\n')
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-    else:
-        print("Invalid arguments")
+  if len(sys.argv) == 5:
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+  else:
+    print("Invalid arguments")
